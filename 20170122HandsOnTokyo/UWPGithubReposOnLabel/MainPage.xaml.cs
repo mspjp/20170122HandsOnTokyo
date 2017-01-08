29d11de8415189e5a001657b541882254bfcddc4
Y&u <willful.slight@gmail.com>
2017-01-09 03:24:14 +0900

    add method  of connect github

--- 20170122HandsOnTokyo/UWPGithubReposOnLabel/MainPage.xaml.cs
+++ 20170122HandsOnTokyo/UWPGithubReposOnLabel/MainPage.xaml.cs
@@ -12,6 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Net;
+using Windows.UI.Popups;
+using System.Net.Http;
+using System.Diagnostics;
 
 // 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
 
@@ -31,7 +35,19 @@ namespace UWPGithubReposOnLabel
         private async void button_Click(object sender, RoutedEventArgs e)
         {
             var dialog = new MessageDialog("Get Github repos", "Get Github repos");      // 直接叩きに行く or EditableBlock作ってもいいかも?
-            await dialog.ShowAsync();         
+            await dialog.ShowAsync();
+            GetGithubRepos();
+
+
+        }
+        public async void GetGithubRepos(/* string userName */) // or not WebClientは使えない
+        {
+            var httpClient = new HttpClient();
+            // Debug.WriteLine(string.Format("https://api.github.com/users/{0}/repos"));
+
+            var content = await  httpClient.GetStringAsync("https://api.github.com/users/r-ralph/repos");
+            
+            // this.label.Text = result;
 
         }
     }
